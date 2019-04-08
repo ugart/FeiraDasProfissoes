ca7659459de4dee2e3f05379142521bea07deffb
JoaoFelipeMaciel <joaofelipemaciel@gmail.com>
2019-04-08 19:06:46 -0300

    telas sorteio, info e splash

---
+++ app/src/main/java/com/example/feiradasprofissoes/SplashScreenActivity.kt
@@ -0,0 +1,15 @@
+package com.example.feiradasprofissoes
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen_2)
+//        setContentView(R.)
+    }
+
+
+}
